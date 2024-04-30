104575a19bc22fe7a9daacd003127d22ded83646
brian-ambani <brainambani1@gmail.com>
2024-04-30 06:43:05 +0300

    add the header section to all pages

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Nav from "../components/Nav";
 export default function Home() {
   return (
     <main className="">
-      <Nav />
       <h1>Home</h1>
     </main>
   );
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,13 +1,3 @@
-import AboutPage from "@/app/about/page";
-import BlogPage from "@/app/blog/page";
-import ProjectsPage from "@/app/projects/page";
-
 export default function Nav() {
-  return (
-    <div>
-      <AboutPage />
-      <ProjectsPage />
-      <BlogPage />
-    </div>
-  );
+  return <div>This is the header section</div>;
 }
